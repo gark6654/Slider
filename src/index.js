8e5ece40622953c2661303f141cc513c053c4107
gark6654 <gark.6654@gmail.com>
2020-05-14 16:09:47 +0400

    Added Readme

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Slider from './slider/Slider';
 
 ReactDOM.render(
-  <Slider autoPlay={true} duration={2}>
+  <Slider autoPlay={false} duration={2}>
       <img alt='img' src='./img/293.jpg' />
       <img alt='img' src='./img/296.jpg' />
       <img alt='img' src='./img/298.jpeg' />
